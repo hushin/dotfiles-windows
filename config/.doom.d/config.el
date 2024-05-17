80846a1b23113ddb88aee071b3c388fc5f948759
hushin <ngtv.hsn@gmail.com>
2024-05-17 22:18:45 +0900

    org-ai など設定

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -50,6 +50,7 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Dropbox/memo/org/")
 (setq org-roam-directory org-directory)
+(setq org-roam-file-exclude-regexp "/archives/")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -166,12 +167,35 @@
           (delete-file filename)
           (message "Deleted file %s" filename)
           (kill-buffer))))))
+;; アーカイブフォルダへ移動
+(defun org-roam-move-file-to-archives ()
+  "Move the current org-roam file to the archives directory, maintaining the directory structure, and open the moved file in a buffer."
+  (interactive)
+  (let* ((file-path (buffer-file-name))
+         (relative-path (file-relative-name file-path org-roam-directory))
+         (archive-path (concat (file-name-as-directory org-roam-directory)
+                               "archives/"
+                               relative-path)))
+    (when (and (org-roam-file-p)
+               (not (string-prefix-p (concat (file-name-as-directory org-roam-directory)
+                                             "archives/")
+                                     file-path)))
+      (mkdir (file-name-directory archive-path) t)
+      (rename-file file-path archive-path)
+      (set-visited-file-name archive-path)
+      (set-buffer-modified-p nil)
+      (when (fboundp 'recentf-add-file)
+        (recentf-add-file archive-path)
+        (recentf-remove-if-non-kept file-path))
+      (message "File moved to archives: %s" archive-path))))
 
 (map! :leader
   (:prefix "f"
     :desc "Move buffer file to directory" "m" #'move-buffer-file-to-directory)
   (:prefix "f"
     :desc "Delete file" "D" #'delete-file-and-buffer)
+  (:prefix "f"
+    :desc "archive file" "A" #'org-roam-move-file-to-archives)
   )
 
 (after! org
@@ -181,7 +205,7 @@
     "C-c j" #'org-journal-new-entry
     )
   (setq org-todo-keywords
-    (quote ((sequence "TODO(t)" "|" "DONE(d)")
+    (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
              (sequence "WAITING(w/!)" "|" "CANCELLED(c/!)"))))
   (setq org-log-done 'time)
   (defun my/property-values-function (property)
@@ -211,7 +235,8 @@ are equal return nil."
                         (format "<%s>" (org-read-date t nil "now"))))
               (cmp (compare-strings a-date nil nil b-date nil nil)))
         (if (eq cmp t) nil (cl-signum cmp)))))
-  
+
+  ;; agenda
   (setq org-agenda-files
     (append
       (directory-files org-directory t "\\.org$")
@@ -297,21 +322,36 @@ Refer to `org-agenda-prefix-format' for more information."
            (org-agenda-view-columns-initially t)
            )
          )
-       ("a" "Default agenda" 
-         (
-           ;; TODO 会議など、今日の calendar 表示
-           (agenda ""
+       ("d" "Done today"
+         ((agenda ""
             (
               (org-agenda-span 'day)
               (org-agenda-log-mode-items '(closed clock))
               (org-agenda-show-log t)
+              ;; (org-agenda-entry-types '(:closed :clock))
+              (org-super-agenda-groups
+                '(
+                   (:name "Done today"
+                     :and (:regexp "State \"DONE\""
+                            :log t))
+                   (:name "Clocked today"
+                     :log t)
+                   (:discard (:anything t))
+                   ))
+              )))
+         )
+       ("a" "Default agenda" 
+         (
+           (todo "NEXT" ((org-agenda-overriding-header "NEXT")))
+           ;; TODO 会議など、今日の calendar 表示
+           (agenda ""
+            (
+              (org-agenda-span 'day)
               (org-super-agenda-groups
                 '(
                    (:name "Today"
                      :scheduled today
                      :deadline today)
-                   (:name "Clocked today"
-                     :log t)
                    (:name "Overdue"
                      :deadline past)
                    (:name "Reschedule"
@@ -357,8 +397,8 @@ Refer to `org-agenda-prefix-format' for more information."
     '(
        ("t" "Task" entry (file+headline "gtd.org" "Inbox")
          "* TODO %? \nCREATED: %U\n %i")
-       ("n" "Task Today" entry (file+headline "gtd.org" "Inbox")
-         "* TODO %? \nCREATED: %U\nSCHEDULED: <%<%Y-%m-%d %a>>\n%i ")
+       ("n" "Task NEXT" entry (file+headline "gtd.org" "Inbox")
+         "* NEXT %? \nCREATED: %U\n %i ")
        ("T" "Task from protocol" entry (file+headline "gtd.org" "Inbox")
          "* TODO %? [[%:link][%:description]] \nCREATED: %U\n%i\n\n")
        ("L" "ReadItLater" entry (file+headline "gtd.org" "ReadItLater")
@@ -438,7 +478,6 @@ Refer to `org-agenda-prefix-format' for more information."
          :unnarrowed t)
        ("p" "project" plain
          "* 目的・目標
-
 - %?
 
 * Tasks
@@ -463,13 +502,17 @@ Refer to `org-agenda-prefix-format' for more information."
        ("r" "rez (Resonance Calendar)" plain "* ${title}
 :PROPERTIES:
 :Type: %?
-:Start: 
+:Start: <%<%Y-%m-%d %a>>
 :Fin: 
 :Canceled: 
 :Rating: 
 :Creator: 
 :URL: 
+:ReleaseDate:
 :END:
+
+** Why
+# なぜ読もう・見ようと思ったのか
 
 ** Tasks
 *** TODO 読む・見る
@@ -487,6 +530,61 @@ Refer to `org-agenda-prefix-format' for more information."
          :unnarrowed t)
        )
     )
+
+  (defun org-roam-protocol-open-ref (info)
+    "Process an org-protocol://roam-ref?ref= style url with INFO."
+    (let ((org-capture-link-is-already-stored t))
+      (org-roam-capture-
+        :keys (plist-get info :template)
+        :node (org-roam-node-create :title (plist-get info :title))
+        :info (list :ref (plist-get info :ref)
+                :body (plist-get info :body)
+                ;; 独自の変数を遅れるように追加
+                :type (plist-get info :type)
+                :creator (plist-get info :creator)
+                :releaseDate (plist-get info :releaseDate)
+                )
+        :templates org-roam-capture-ref-templates))
+    nil)
+  (setq org-roam-capture-ref-templates
+    '(
+       ("r" "ref" plain
+         "%?"
+         :target (file+head "resources/ref/%<%Y%m%d%H%M%S>-${slug}.org"
+                   "#+title: ${title}\n${ref}\n${body}")
+         :unnarrowed t)
+       ("z" "rez (Resonance Calendar) from protocol" plain "%?"
+         :target (file+head "resources/rez/%<%Y%m%d%H%M%S>-${slug}.org"
+                   "#+title: ${title}
+* ${title}
+:PROPERTIES:
+:Type: ${type}
+:Start: <%<%Y-%m-%d %a>>
+:Fin: 
+:Canceled: 
+:Rating: 
+:Creator: ${creator}
+:URL: ${ref}
+:ReleaseDate: ${releaseDate}
+:END:
+
+** Why
+# なぜ読もう・見ようと思ったのか
+
+** Tasks
+*** TODO 読む・見る
+
+** Key Ideas
+
+** Review
+
+** Quotes
+
+** Notes
+
+")
+         :unnarrowed t)
+       ))
   )
 
 (use-package! org-super-agenda
@@ -500,3 +598,31 @@ Refer to `org-agenda-prefix-format' for more information."
 (use-package! org-roam-timestamps
   :after org-roam
   :config (org-roam-timestamps-mode))
+
+;; GnuPG for auth-source
+(when (winp)
+  (custom-set-variables
+    '(epg-gpg-home-directory "~/scoop/apps/gpg/current/home")
+    '(epg-gpg-program "~/scoop/apps/gpg/current/bin/gpg.exe")
+    '(epg-gpgconf-program "~/scoop/apps/gpg/current/bin/gpgconf.exe")
+    )
+  )
+(use-package! org-ai
+  :after org
+  :commands (org-ai-mode)
+  :init
+  (add-hook 'org-mode-hook #'org-ai-mode) ; enable org-ai in org-mode
+  ;; https://github.com/rksm/org-ai/issues/18#issuecomment-1737931580
+  (defun my/org-ai-after-chat-insertion-hook (type _text)
+    (when (and (eq type 'end) (eq major-mode 'org-mode) (memq 'org-indent-mode minor-mode-list))
+      (org-indent-indent-buffer)))
+  (add-hook 'org-ai-after-chat-insertion-hook #'my/org-ai-after-chat-insertion-hook)
+  ;; warning 抑制
+  (defun my/suppress-org-ai-warnings (old-fun &rest args)
+    (with-demoted-errors "org-ai: %S"
+      (apply old-fun args)))
+  (advice-add 'org-element-cache-reset :around #'my/suppress-org-ai-warnings)
+  :config
+  (setq org-ai-default-chat-model "gpt-4o")
+  )
+(setq markdown-fontify-code-blocks-natively t)
--- config/.doom.d/packages.el
+++ config/.doom.d/packages.el
@@ -11,7 +11,7 @@
 (package! super-save)
 (package! org-roam-timestamps)
 (package! org-super-agenda)
-
+(package! org-ai)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
