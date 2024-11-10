81a7bb0c753d558a6f5c5ed43ddc61529c1db662
hushin <ngtv.hsn@gmail.com>
2024-11-10 09:54:46 +0900

    C-S-Enter でのタイムスタンプをトグルできるように改善

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -440,22 +440,32 @@ Refer to `org-agenda-prefix-format' for more information."
        ))
 
   ;; C-S-Enter で見出し作ったときにタイムスタンプをつける
-  (defun my/org-insert-created-timestamp ()
+  (defun my/insert-created-timestamp ()
     "Insert timestamp as plain text after heading"
-    (interactive)
-    (let ((heading-pos (point)))  ; 現在位置（見出し）を保存
+    (save-excursion 
       (org-back-to-heading t)
       (end-of-line)
-      (insert "\nCREATED: " (format-time-string "[%Y-%m-%d %a %H:%M]"))
-      (goto-char heading-pos)     ; 元の位置（見出し末尾）に戻る
-      (end-of-line)))            ; 見出しの末尾に移動
-  (defun my/org-insert-todo-with-timestamp (&rest _)
-    "Advice function to add timestamp after creating a TODO heading"
-    (when (org-at-heading-p)
-      (my/org-insert-created-timestamp)))
+      (insert "\nCREATED: " (format-time-string "[%Y-%m-%d %a %H:%M]"))))
 
-  (advice-add 'org-insert-todo-heading-respect-content
-    :after #'my/org-insert-todo-with-timestamp)
+  (defun my/org-mode-timestamp-on ()
+     "Enable automatic timestamp insertion for new headings"
+     (add-hook 'org-insert-heading-hook #'my/insert-created-timestamp))
+
+  (defun my/org-mode-timestamp-off ()
+   "Disable automatic timestamp insertion for new headings"
+   (remove-hook 'org-insert-heading-hook #'my/insert-created-timestamp))
+
+  (defun my/org-mode-timestamp-toggle ()
+   "Toggle automatic timestamp insertion for new headings"
+   (interactive)
+   (if (memq #'my/insert-created-timestamp org-insert-heading-hook)
+     (progn
+       (my/org-mode-timestamp-off)
+       (message "Timestamp insertion disabled"))
+     (progn
+       (my/org-mode-timestamp-on)
+       (message "Timestamp insertion enabled"))))
+  (global-set-key (kbd "C-c C-x t") #'my/org-mode-timestamp-toggle)
   
   ;; journal
   (setq org-journal-file-format "%Y-%m-%d")
