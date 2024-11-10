fdeefccc14bdf2ed712b3f7d1e457cc7ba2aa6b8
hushin <ngtv.hsn@gmail.com>
2024-11-10 10:12:21 +0900

    fix timestamp

--- config/.doom.d/config.el
+++ config/.doom.d/config.el
@@ -440,31 +440,35 @@ Refer to `org-agenda-prefix-format' for more information."
        ))
 
   ;; C-S-Enter で見出し作ったときにタイムスタンプをつける
-  (defun my/insert-created-timestamp ()
+  (defun my/org-insert-created-timestamp ()
     "Insert timestamp as plain text after heading"
-    (save-excursion 
+    (interactive)
+    (let ((heading-pos (point)))  ; 現在位置（見出し）を保存
       (org-back-to-heading t)
       (end-of-line)
-      (insert "\nCREATED: " (format-time-string "[%Y-%m-%d %a %H:%M]"))))
-
+      (insert "\nCREATED: " (format-time-string "[%Y-%m-%d %a %H:%M]"))
+      (goto-char heading-pos)     ; 元の位置（見出し末尾）に戻る
+      (end-of-line)))            ; 見出しの末尾に移動
   (defun my/org-mode-timestamp-on ()
-     "Enable automatic timestamp insertion for new headings"
-     (add-hook 'org-insert-heading-hook #'my/insert-created-timestamp))
+     "Enable automatic timestamp insertion for new todo headings"
+     (advice-add 'org-insert-todo-heading-respect-content
+       :after #'my/org-insert-created-timestamp))
 
   (defun my/org-mode-timestamp-off ()
-   "Disable automatic timestamp insertion for new headings"
-   (remove-hook 'org-insert-heading-hook #'my/insert-created-timestamp))
+     "Disable automatic timestamp insertion for new todo headings"
+     (advice-remove 'org-insert-todo-heading-respect-content
+       #'my/org-insert-created-timestamp))
 
   (defun my/org-mode-timestamp-toggle ()
-   "Toggle automatic timestamp insertion for new headings"
-   (interactive)
-   (if (memq #'my/insert-created-timestamp org-insert-heading-hook)
-     (progn
-       (my/org-mode-timestamp-off)
-       (message "Timestamp insertion disabled"))
-     (progn
-       (my/org-mode-timestamp-on)
-       (message "Timestamp insertion enabled"))))
+     "Toggle automatic timestamp insertion for new todo headings"
+     (interactive)
+     (if (advice-member-p #'my/org-insert-created-timestamp 'org-insert-todo-heading-respect-content)
+       (progn
+         (my/org-mode-timestamp-off)
+         (message "Timestamp insertion disabled"))
+       (progn
+         (my/org-mode-timestamp-on)
+         (message "Timestamp insertion enabled"))))
   (global-set-key (kbd "C-c C-x t") #'my/org-mode-timestamp-toggle)
   
   ;; journal
